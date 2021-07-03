0b0790a2c922c2179c0d7dd34317814525264bd3
16519111 <gregdimasbaskara@gmail.com>
2021-07-03 14:58:58 +0700

    add navbar

--- src/views/nav/Navbar.vue
+++ src/views/nav/Navbar.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="nav">
-    <router-link class="nav-link" to="/">Home</router-link> |
-    <router-link class="nav-link" to="/about">About</router-link>
+    <div class="nav-left">
+      <div class="nav-link">
+        ICON
+      </div>
+      <router-link class="nav-link" to="/">HOME</router-link>
+      <router-link class="nav-link" to="/">PAGE #</router-link>
+      <router-link class="nav-link" to="/">PAGE #</router-link>
+    </div>
+    <div class="nav-right">
+      <div class="search-box">
+        <font-awesome-icon icon="search"/>
+        <input type="text" class="search-input" placeholder="Search your vacation ...">
+      </div>
+      <button class="button">SIGN UP</button>
+    </div>
   </div>
 </template>
 
@@ -14,9 +27,55 @@ export default {
 <style scoped>
 .nav {
   position: sticky;
-  width: 100%;
   height: 50px;
   background: #fff;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5%;
+}
+
+.nav-left {
+  display: flex;
+}
+
+.nav-right {
+  display: flex;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: black;
+  margin-right: 40px;
+}
+
+.search-box {
+  background: #E5F1FF;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  color: #8CB9FD;
+  border-radius: 5px;
+}
+
+.search-input {
+  background: transparent;
+  border: none;
+  margin-left: 5px;
+}
+
+.search-input::placeholder {
+  color: #8CB9FD;
+}
+
+.button {
+  color: white;
+  background: #2759AA;
+  padding: 6px 15px;
+  border: none;
+  border-radius: 5px;
+  white-space: nowrap;
+  margin-left: 15px;
 }
 </style>
