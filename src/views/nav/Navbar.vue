33fc9f77e2cfcaeab940cd273abc12ce4e928fc8
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-07-03 14:41:40 +0800

    feat(Nav): add navbar wireframe

---
+++ src/views/nav/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav">
+    <router-link class="nav-link" to="/">Home</router-link> |
+    <router-link class="nav-link" to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.nav {
+  position: sticky;
+  width: 100%;
+  height: 50px;
+  background: #fff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
