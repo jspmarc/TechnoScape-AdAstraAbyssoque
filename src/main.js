d52a5ba50fcafce149a07e1cb58d54152603a8f6
Josep Marcello <jspmarcello@live.com>
2021-07-03 13:40:02 +0700

    tambah font awesome

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import { createApp } from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import store from "./store";
 import router from "./router";
+import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSearch);
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
 
 app.config.globalProperties.$store = store;
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount("#app");
